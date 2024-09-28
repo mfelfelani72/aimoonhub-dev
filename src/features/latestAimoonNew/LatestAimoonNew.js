f0fb2d84f0611eb8c8026141d9dcd1ac3e8e85f2
mohammad <mfelfelani72@gmail.com>
2024-09-28 16:45:17 +0330

    mohammad

--- src/features/latestAimoonNew/LatestAimoonNew.js
+++ src/features/latestAimoonNew/LatestAimoonNew.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-import { Autoplay, A11y } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import "swiper/css/autoplay";
-import "swiper/css/controller";
+import "swiper/css/pagination";
+
+import { Pagination } from "swiper/modules";
 
 import avatar from "../../../assets/images/avatar.png";
 
@@ -36,39 +36,47 @@ const LatestAimoonNew = () => {
         </div>
       </div>
 
-      <div className="bg-slate-50 p-4">
+      <div className="bg-slate-50 p-4 pb-0">
         <Swiper
-          modules={[Autoplay, A11y]}
-          speed={5000}
-          effect="fade"
-          spaceBetween={30} // Adjust the spacing as needed
-          slidesPerView={1} // Show only one slide by default
-          autoplay={{
-            delay: 10000,
-            disableOnInteraction: false,
+          slidesPerView={"auto"}
+          spaceBetween={30}
+          pagination={{
+            clickable: true,
           }}
+          style={{
+            "--swiper-pagination-color": "#e57c43",
+            "--swiper-pagination-bullet-inactive-color": "#999999",
+            "--swiper-pagination-bullet-inactive-opacity": "1",
+            "--swiper-pagination-bullet-size": "10px",
+            "--swiper-pagination-bullet-horizontal-gap": "5px",
+          }}
+          modules={[Pagination]}
         >
           <SwiperSlide>
-            <div className="relative">
+            <div className="relative h-[10rem]">
               <div className="h-[8.5rem]">
-                <img src={defaultImage} />
-              </div>
-              <div className="absolute right-0 top-0 m-2 bg-D-color-theme">
-                <div className="p-[0.2rem] text-[0.8rem] font-bold text-white">News</div>
-              </div>
-              <div className="absolute right-0 bottom-0 m-2 h-[5rem] w-[20rem] border-D-color-theme rounded bg-white border-l-4 border-t-4">
-                <div className="flex flex-col pb-1">
-                  <h2 className="pt-2 px-2 font-bold text-[0.9rem]">
-                  Federal Court Fined New York Man $36 Million for Crypto Fraud 
-                  </h2>
-                  <div className="flex flex-row pt-1 pl-2 items-center">
-                    <div className="flex basis-1/2">
-                      <img src={avatar} className="h-5 w-5 rounded-[30px]" />
-                      <span className="px-1 text-[0.7rem]">author name</span>
-                    </div>
+                <img className="h-[8.5rem] w-full" src={defaultImage} />
 
-                    <div className="basis-1/2 text-end">
-                      <div className="pr-2 text-[0.8rem]">2 days ago</div>
+                <div className="absolute right-0 top-0 m-2 bg-D-color-theme">
+                  <div className="p-[0.2rem] text-[0.8rem] font-bold text-white">
+                    News
+                  </div>
+                </div>
+                <div className="absolute right-0 bottom-2 m-2 mb-6 h-[5rem] w-[20rem] border-D-color-theme rounded bg-white border-l-4 border-t-4">
+                  <div className="flex flex-col pb-1">
+                    <h2 className="pt-2 px-2 font-bold text-[0.9rem]">
+                      Federal Court Fined New York Man $36 Million for Crypto
+                      Fraud
+                    </h2>
+                    <div className="flex flex-row pt-1 pl-2 items-center">
+                      <div className="flex basis-1/2">
+                        <img src={avatar} className="h-5 w-5 rounded-[30px]" />
+                        <span className="px-1 text-[0.7rem]">author name</span>
+                      </div>
+
+                      <div className="basis-1/2 text-end">
+                        <div className="pr-2 text-[0.8rem]">2 days ago</div>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -76,26 +84,30 @@ const LatestAimoonNew = () => {
             </div>
           </SwiperSlide>
           <SwiperSlide>
-            <div className="relative">
+            <div className="relative h-[10rem]">
               <div className="h-[8.5rem]">
-                <img src={defaultImage} />
-              </div>
-              <div className="absolute right-0 top-0 m-2 bg-D-color-theme">
-                <div className="p-[0.2rem] text-[0.8rem] font-bold text-white">News</div>
-              </div>
-              <div className="absolute right-0 bottom-0 m-2 h-[5rem] w-[20rem] border-D-color-theme rounded bg-white border-l-4 border-t-4">
-                <div className="flex flex-col pb-1">
-                  <h2 className="pt-2 px-2 font-bold text-[0.9rem]">
-                  Federal Court Fined New York Man $36 Million for Crypto Fraud 
-                  </h2>
-                  <div className="flex flex-row pt-1 pl-2 items-center">
-                    <div className="flex basis-1/2">
-                      <img src={avatar} className="h-5 w-5 rounded-[30px]" />
-                      <span className="px-1 text-[0.7rem]">author name</span>
-                    </div>
+                <img className="h-[8.5rem] w-full" src={defaultImage} />
 
-                    <div className="basis-1/2 text-end">
-                      <div className="pr-2 text-[0.8rem]">2 days ago</div>
+                <div className="absolute right-0 top-0 m-2 bg-D-color-theme">
+                  <div className="p-[0.2rem] text-[0.8rem] font-bold text-white">
+                    News
+                  </div>
+                </div>
+                <div className="absolute right-0 bottom-2 m-2 mb-6 h-[5rem] w-[20rem] border-D-color-theme rounded bg-white border-l-4 border-t-4">
+                  <div className="flex flex-col pb-1">
+                    <h2 className="pt-2 px-2 font-bold text-[0.9rem]">
+                      Federal Court Fined New York Man $36 Million for Crypto
+                      Fraud
+                    </h2>
+                    <div className="flex flex-row pt-1 pl-2 items-center">
+                      <div className="flex basis-1/2">
+                        <img src={avatar} className="h-5 w-5 rounded-[30px]" />
+                        <span className="px-1 text-[0.7rem]">author name</span>
+                      </div>
+
+                      <div className="basis-1/2 text-end">
+                        <div className="pr-2 text-[0.8rem]">2 days ago</div>
+                      </div>
                     </div>
                   </div>
                 </div>
