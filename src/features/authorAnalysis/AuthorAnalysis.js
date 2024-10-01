79156b75e7150e7c5c777f1b60b6ed55cf1d6e8c
mohammad <mfelfelani72@gmail.com>
2024-10-01 15:21:20 +0330

    sadsa

--- src/features/authorAnalysis/AuthorAnalysis.js
+++ src/features/authorAnalysis/AuthorAnalysis.js
@@ -46,6 +46,7 @@ function AuthorAnalysis() {
       {/* title */}
 
       <div className="container mx-auto">
+        {/* authors images */}
         <div className="self-center pl-2">
           <div className="text-[0.8rem] font-bold pb-1">Top 10 Authors</div>
           <div className="flex flex-row items-center">
@@ -63,32 +64,39 @@ function AuthorAnalysis() {
               ))}
             </div>
             <div className="basis-1/4 text-center">
-              <Button className="bg-color-theme hover:bg-color-theme-light text-[0.8rem] h-7 cursor-pointer">
-                Author List
+              <Button className="bg-color-theme hover:bg-color-theme-light text-[0.75rem] h-7 cursor-pointer">
+                Authors List
               </Button>
             </div>
           </div>
         </div>
+        {/* authors images */}
+        
         <div className="flex flex-row mt-4 p-2">
-          <div className="basis-2/5 text-md content-center text-T-bright">
-            <div className="text-lime-500 text-center">
-              <span className="font-bold">+{totalStat?.total_author}</span>{" "}
-              Total Author
+          <div className="basis-1/2 text-md content-center text-T-bright">
+            <div className="h-[9rem] w-[9rem] mx-auto -mt-3 mb-2">
+              <ChartPie
+                data={[
+                  totalStat?.crypto_author_counts,
+                  totalStat?.forex_author_counts,
+                  totalStat?.commodity_author_counts,
+                ]}
+                colors={["#fde047", "#06b6d4", "#a855f7"]}
+                //   labels={[`+${totalStat?.crypto_author_counts} Cryptocurrencies`,`+${totalStat?.forex_author_counts} Forex`,`+${totalStat?.commodity_author_counts} Commodity`]}
+              />
             </div>
-            {/* <div>
-            +{totalStat?.crypto_author_counts} Author in Cryptocurrencies
+            <div className="text-[0.67rem] text-center font-bold">
+              Author distribution chart in categories
+            </div>
           </div>
-          <div>+{totalStat?.forex_author_counts} Author in Forex</div>
-          <div>+{totalStat?.commodity_author_counts} Author in Commodity</div> */}
-          </div>
-          <div className="basis-3/5">
+          <div className="basis-1/2">
             <div className="flex flex-col content-center justify-center">
               <div className="basis-1/2">
                 <div className="flex flex-row">
                   <div className="basis-1/4">
-                    <div className="bg-[#fde047] w-[2rem] h-2 my-1 rounded-sm"></div>
-                    <div className="bg-[#06b6d4] w-[2rem] h-2 my-3 rounded-sm"></div>
-                    <div className="bg-[#a855f7] w-[2rem] h-2 my-1 rounded-sm"></div>
+                    <div className="bg-[#fde047] w-[2rem] h-2 my-1"></div>
+                    <div className="bg-[#06b6d4] w-[2rem] h-2 my-3"></div>
+                    <div className="bg-[#a855f7] w-[2rem] h-2 my-1"></div>
                   </div>
                   <div className="basis-3/4 text-[0.85rem] text-slate-800">
                     <div>
@@ -108,21 +116,16 @@ function AuthorAnalysis() {
                       <span className="font-bold">
                         +{totalStat?.commodity_author_counts}
                       </span>{" "}
-                      Commodity
+                      Commodities
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="basis-1/2 h-[9rem] w-[9rem] mx-auto">
-                <ChartPie
-                  data={[
-                    totalStat?.crypto_author_counts,
-                    totalStat?.forex_author_counts,
-                    totalStat?.commodity_author_counts,
-                  ]}
-                  colors={["#fde047", "#06b6d4", "#a855f7"]}
-                  //   labels={[`+${totalStat?.crypto_author_counts} Cryptocurrencies`,`+${totalStat?.forex_author_counts} Forex`,`+${totalStat?.commodity_author_counts} Commodity`]}
-                />
+              <div className="basis-1/2">
+                <div className="text-lime-500 text-center text-lg mt-8">
+                  <span className="font-bold">+{totalStat?.total_author}</span>{" "}
+                  Total Author
+                </div>
               </div>
             </div>
           </div>
