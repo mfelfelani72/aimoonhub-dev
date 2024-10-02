bba011d3f812d83c5d97f618e2a0104b6cf05e55
mohammad <mfelfelani72@gmail.com>
2024-10-02 14:55:05 +0330

    asdsa

--- src/features/authorAnalysis/AuthorDashboard.js
+++ src/features/authorAnalysis/AuthorDashboard.js
@@ -6,7 +6,6 @@ import avatar from "../../../assets/images/avatar.png";
 import { getData } from "../../../utils/helpers/getData";
 import { AUTHOR_INFO } from "../../app/constant/EndPoints";
 function AuthorDashboard() {
-
   const [author, setAuthor] = useState();
   const [category, setCategory] = useState("cryptocurrencies");
   const [authorName, setAuthorName] = useState("newsbtc");
@@ -35,13 +34,13 @@ function AuthorDashboard() {
   }, [author]);
 
   let defaultImage =
-  "https://cdn3d.iconscout.com/3d/premium/thumb/bitcoin-3d-illustration-download-in-png-blend-fbx-gltf-file-formats--logo-btc-gold-symbol-sign-crpto-glossy-crypto-pack-science-technology-illustrations-3591010.png?f=webp";
+    "https://cdn3d.iconscout.com/3d/premium/thumb/bitcoin-3d-illustration-download-in-png-blend-fbx-gltf-file-formats--logo-btc-gold-symbol-sign-crpto-glossy-crypto-pack-science-technology-illustrations-3591010.png?f=webp";
   return (
     <>
       <h3 className="p-2">AuthorDashboard</h3>
       <div className="container mx-auto mb-3">
         <div className="flex">
-          <div className="basis-1/3 mx-auto">
+          <div className="basis-1/5 self-center mx-auto">
             <div className="">
               <a href={author?.biographyUrl} target="_blank">
                 <img
@@ -56,46 +55,60 @@ function AuthorDashboard() {
               </a>
             </div>
           </div>
-          <div className="basis-1/3 self-center">
-            <div className="text-[0.8rem] text-slate-800 pt-1 text-center">
+          <div className="basis-3/5 ">
+            <div className="text-[0.8rem] text-slate-800 pt-1 px-2">
+              <span className="text-[0.8rem] font-bold">Biography</span>
               <a href="{author?.biographyUrl}" target="_blank">
-                <div className="text-[0.7rem] text-justify">{author?.biography}</div>
+                <div className="text-[0.7rem] text-justify">
+                  {author?.biography}
+                </div>
               </a>
             </div>
           </div>
-          <div className="basis-1/3 self-center max-h-[7rem] overflow-y-auto border-2 shadow-inner bg-slate-50 mx-2">
-           <div className="flex">
-            <img className="h-7 w-7" src={defaultImage} />
-            <div className="text-[0.7rem] m-2">name</div>
-           </div>
-           <div className="flex">
-            <img className="h-7 w-7" src={defaultImage} />
-            <div className="text-[0.7rem] m-2">name</div>
-           </div>
-           <div className="flex">
-            <img className="h-7 w-7" src={defaultImage} />
-            <div className="text-[0.7rem] m-2">name</div>
-           </div>
-           <div className="flex">
-            <img className="h-7 w-7" src={defaultImage} />
-            <div className="text-[0.7rem] m-2">name</div>
-           </div>
-           <div className="flex">
-            <img className="h-7 w-7" src={defaultImage} />
-            <div className="text-[0.7rem] m-2">name</div>
-           </div>
-           <div className="flex">
-            <img className="h-7 w-7" src={defaultImage} />
-            <div className="text-[0.7rem] m-2">name</div>
-           </div>
-           <div className="flex">
-            <img className="h-7 w-7" src={defaultImage} />
-            <div className="text-[0.7rem] m-2">name</div>
-           </div>
-           <div className="flex">
-            <img className="h-7 w-7" src={defaultImage} />
-            <div className="text-[0.7rem] m-2">name</div>
-           </div>
+          <div className="basis-1/5 self-center mx-2">
+            <span className="text-[0.8rem] font-bold">Coins</span>
+            <div className="max-h-[6rem] overflow-y-auto border-2 shadow-inner bg-slate-50">
+              <div className="flex">
+                <img className="h-7 w-7" src={defaultImage} />
+                <div className="text-[0.7rem] mt-2">name</div>
+              </div>
+              <div className="flex">
+                <img className="h-7 w-7" src={defaultImage} />
+                <div className="text-[0.7rem] mt-2">name</div>
+              </div>
+              <div className="flex">
+                <img className="h-7 w-7" src={defaultImage} />
+                <div className="text-[0.7rem] mt-2">name</div>
+              </div>
+              <div className="flex">
+                <img className="h-7 w-7" src={defaultImage} />
+                <div className="text-[0.7rem] mt-2">name</div>
+              </div>
+              <div className="flex">
+                <img className="h-7 w-7" src={defaultImage} />
+                <div className="text-[0.7rem] mt-2">name</div>
+              </div>
+              <div className="flex">
+                <img className="h-7 w-7" src={defaultImage} />
+                <div className="text-[0.7rem] mt-2">name</div>
+              </div>
+              <div className="flex">
+                <img className="h-7 w-7" src={defaultImage} />
+                <div className="text-[0.7rem] mt-2">name</div>
+              </div>
+              <div className="flex">
+                <img className="h-7 w-7" src={defaultImage} />
+                <div className="text-[0.7rem] mt-2">name</div>
+              </div>
+              <div className="flex">
+                <img className="h-7 w-7" src={defaultImage} />
+                <div className="text-[0.7rem] mt-2">name</div>
+              </div>
+              <div className="flex">
+                <img className="h-7 w-7" src={defaultImage} />
+                <div className="text-[0.7rem] mt-2">name</div>
+              </div>
+            </div>
           </div>
         </div>
         <div className="flex">
