650ff23be28ad098c8d6b16e905efc0788a56b0a
mohammad <mfelfelani72@gmail.com>
2024-10-12 13:41:12 +0330

    edit

--- src/features/author/components/CardRow.jsx
+++ src/features/author/components/CardRow.jsx
@@ -13,7 +13,7 @@ const CardRow = (props) => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex flex-row px-2 ltr:md:pr-6 rtl:md:pl-6 ltr:bi:pr-6 rtl:bi:pl-6 rtl:lg:px-6 pt-2 pb-2 border-b border-color-theme-light dark:border-D-color-theme-light">
+    <div className="flex flex-row p-2 border-b border-color-theme-light dark:border-D-color-theme-light">
       {/* image */}
       <div className="w-[8rem] h-[6rem]">
         <a href={props.row.link} target="_blank">
@@ -73,7 +73,7 @@ const CardRow = (props) => {
         <div className="px-2 text-end">
           <span className="px-1 text-[0.7rem] text-slate-500">
             {" "}
-            {dateHelper(props.row.pubDate)}
+            {dateHelper(props.row.pubDate, "difference")}
           </span>
         </div>
         {/* date */}
