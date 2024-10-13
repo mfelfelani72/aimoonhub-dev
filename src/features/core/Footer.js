815b07558594b24a54da5c1525c990c87f4b5aaa
mohammad <mfelfelani72@gmail.com>
2024-10-13 12:48:51 +0330

    update

--- src/features/core/Footer.js
+++ src/features/core/Footer.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 
 import { getData } from "../../../utils/helpers/getData.js";
-import {goToSymbolDashboard} from "../../../utils/lib/symbol/goToSymbolDashboard.js"
+import { goToSymbolDashboard } from "../../../utils/lib/symbol/goToSymbolDashboard.js";
 
 import { SYMBOLS } from "../../app/constant/EndPoints.js";
 import { DEFAULT_COIN_IMAGE } from "../../app/constant/Defaults.js";
@@ -13,6 +13,7 @@ import { Navigation } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
+import Button from "./components/Button.jsx";
 
 export function Footer() {
   const { t } = useTranslation();
@@ -31,7 +32,7 @@ export function Footer() {
       getData(SYMBOLS, parameter).then((response) => {
         if (response.data.data) {
           console.log("Fetch dataSymbolsList done.");
-        //   console.log(response.data.data);
+          //   console.log(response.data.data);
           setSymbolsList(response.data.data);
         }
       });
@@ -47,38 +48,51 @@ export function Footer() {
   return (
     <>
       <footer className="relative border-t border-color-theme-light dark:border-D-color-theme-light bg-B-bright dark:bg-DT-dim z-10">
-        <div className="flex flex-row m-3 p-2 border border-slate-300 rounded-xl">
-          <Swiper
-            modules={[Navigation]}
-            spaceBetween={1}
-            slidesPerView={4}
-            navigation
-          >
-            {symbolsList.map((row, index) => (
-              <SwiperSlide key={index}>
-                <>
-                  <div className="flex justify-center">
-                    <img src={row?.logo} className="h-6 w-6 rounded-full"></img>
-                  </div>
-                  <div className="pt-1 text-center text-[0.7rem] font-bold">
-                    <a
-                      className="cursor-pointer hover:text-color-theme"
-                      onClick={(event) =>
-                        goToSymbolDashboard(
-                          navigate,
-                          event,
-                          row?.name,
-                          nav
-                        )
-                      }
-                    >
-                      {row?.name}
-                    </a>
-                  </div>
-                </>
-              </SwiperSlide>
-            ))}
-          </Swiper>
+        <div className="flex flex-col m-3 p-2 border border-slate-300 rounded-xl">
+          <div className="flex flex-row">
+            <Swiper
+              modules={[Navigation]}
+              spaceBetween={1}
+              slidesPerView={4}
+              navigation
+            >
+              {symbolsList.map((row, index) => (
+                <SwiperSlide key={index}>
+                  <>
+                    <div className="flex justify-center">
+                      <img
+                        src={row?.logo}
+                        className="h-6 w-6 rounded-full"
+                      ></img>
+                    </div>
+                    <div className="pt-1 text-center text-[0.7rem] font-bold">
+                      <a
+                        className="cursor-pointer hover:text-color-theme"
+                        onClick={(event) =>
+                          goToSymbolDashboard(navigate, event, row?.name, nav)
+                        }
+                      >
+                        {row?.name}
+                      </a>
+                    </div>
+                  </>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+          <div className="mt-4">
+            <Button
+              className="bg-color-theme hover:bg-color-theme dark:bg-D-color-theme dark:hover:bg-D-color-theme text-[0.8rem] px-3"
+              onClick={(event) => {
+                event.preventDefault();
+                navigate("/symbols-list", {
+                  state: { nav: nav },
+                });
+              }}
+            >
+              Symbols List
+            </Button>
+          </div>
         </div>
         <div className="h-16 py-2">
           <div className="text-sm/6  text-center flex-col ltr">
